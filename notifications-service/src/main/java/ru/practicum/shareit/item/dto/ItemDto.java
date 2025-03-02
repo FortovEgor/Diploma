ac41757533148bc9c7429850225726ebac196992
Egor Fortov <egorfortov@gmail.com>
2025-03-02 15:11:31 +0300

    first touch

---
+++ notifications-service/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -0,0 +1,25 @@
+package ru.practicum.shareit.item.dto;
+
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * TODO Sprint add-controllers.
+ */
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemDto {
+    private Long id;
+    private String name;
+    private String description;
+    private boolean available;
+    private List<CommentDto> comments;
+    private LocalDateTime lastBooking;
+    private LocalDateTime nextBooking;
+}
