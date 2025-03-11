f25db26fecc9b910780239af96bce9b8812f5003
Egor Fortov <egorfortov@gmail.com>
2025-03-11 21:36:25 +0300

    duties service fixes

--- duties-service/src/main/java/fortov/egor/diploma/dto/CreateDutyRequest.java
+++ duties-service/src/main/java/fortov/egor/diploma/dto/CreateDutyRequest.java
@@ -3,6 +3,7 @@ package fortov.egor.diploma.dto;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Data
@@ -11,7 +12,7 @@ public class CreateDutyRequest {
     LocalDateTime start_time;
 
     @NotNull(message = "Интервал необходим для определения времени работы одного дежурного")
-    Long interval;
+    Duration interval;
 
     @NotNull(message = "Должен быть назначен список дежурных")
     Long[] ids;
--- duties-service/src/main/java/fortov/egor/diploma/dto/UpdateDutyRequest.java
+++ duties-service/src/main/java/fortov/egor/diploma/dto/UpdateDutyRequest.java
@@ -3,13 +3,14 @@ package fortov.egor.diploma.dto;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Data
 public class UpdateDutyRequest {
     LocalDateTime start_time;
 
-    Long interval;
+    Duration interval;
 
     Long[] ids;
 }
