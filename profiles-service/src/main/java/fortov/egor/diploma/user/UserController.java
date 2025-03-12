202fad0ea0873126714fd86e6e1a1c5dc22daa67
Egor Fortov <egorfortov@gmail.com>
2025-03-12 13:54:49 +0300

    test fixes

--- profiles-service/src/main/java/fortov/egor/diploma/user/UserController.java
+++ profiles-service/src/main/java/fortov/egor/diploma/user/UserController.java
@@ -6,6 +6,7 @@ import fortov.egor.diploma.user.dto.UserDtoPartial;
 import fortov.egor.diploma.user.dto.UserFullInfoDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
