32361e70543089537e491ea0cbc5382059ca38cf
Egor Fortov <egorfortov@gmail.com>
2025-04-18 09:58:25 +0300

    intergrated aero sms into sms sending service

--- notifications-service/src/main/java/fortov/egor/diploma/sendingServices/SmsService.java
+++ notifications-service/src/main/java/fortov/egor/diploma/sendingServices/SmsService.java
@@ -2,52 +2,62 @@ package fortov.egor.diploma.sendingServices;
 
 import fortov.egor.diploma.user.UserFullInfoDto;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
+import ru.smsaero.SmsAero;
 
 @Slf4j
 @Service
 public class SmsService extends SendingServiceTemplate {
+    @Value("${aero-sms.email}")
+    private String aeroEmail;
 
-    @Value("${twilio.account-sid}")
-    private String accountSid;
+    @Value("${aero-sms.key}")
+    private String aeroKey;
 
-    @Value("${twilio.auth-token}")
-    private String authToken;
-
-    @Value("${twilio.phone-number}")
-    private String twilioPhoneNumber;
+    private static String header = "Привет! На связи Argus. У нас для тебя уведомление. ";
 
     public void sendSms(String to, String messageBody) {
-        Message message = Message.creator(
-                new PhoneNumber(to), // номер получателя
-                new PhoneNumber(twilioPhoneNumber), // ваш номер Twilio
-                messageBody // текст сообщения
-        ).create();
-
-        System.out.println("SMS sent: " + message.getSid());
+        String phone = formatPhoneNumber(to);
+        if (phone == null) {
+            log.error("sms to {} was not sent due to internal error while phone handling", to);
+            return;
+        }
+        try {
+            SmsAero client = new SmsAero(aeroEmail, aeroKey);
+            JSONObject sendResult = client.SendSms(phone, header + messageBody);
+            log.info("result of sending sms to {}: {}", to, sendResult);
+        } catch (Exception e) {
+            log.info("sending sms to {} failed: {}", to, e.getMessage());
+        }
     }
 
+    private String formatPhoneNumber(String phoneNumber) {
+        // Удаляем все символы, кроме цифр
+        String cleaned = phoneNumber.replaceAll("[^\\d]", "");
+
+        // Проверяем, начинается ли номер с 8 или 7 и заменяем на 7
+        if (cleaned.startsWith("8")) {
+            cleaned = "7" + cleaned.substring(1);
+        } else if (cleaned.startsWith("7")) {
+
+        } else if (cleaned.length() == 10) {
+            // Если номер состоит из 10 цифр, добавляем 7 в начале
+            cleaned = "7" + cleaned;
+        } else {
+            // Если номер не соответствует ожидаемым форматам, возвращаем null или выбрасываем исключение
+            return null;
+        }
+
+        return cleaned;
+    }
 
     SmsService() {
         super("sms");
-        try {
-            Twilio.init(accountSid, authToken);
-        } catch (RuntimeException e) {
-            log.error("Failed to init Twilio connection: " + e +
-                    " | call & sms notifications will not be delivered");
-        }
     }
     @Override
     public void send(UserFullInfoDto receiver, String content) {
-        Message message = Message.creator(
-                new PhoneNumber(receiver.getPhone()),
-                new PhoneNumber(twilioPhoneNumber),
-                content
-        ).create();
-        log.debug("message with content {} sent to {}", content, receiver);
+        sendSms(receiver.getPhone(), content);
     }
 }
