31a2c405b3ff813cae621110e5be211e82a51689
Egor Fortov <egorfortov@gmail.com>
2025-05-15 10:19:27 +0300

    added Docs, updated postman tests

--- profiles-service/src/main/resources/schema.sql
+++ profiles-service/src/main/resources/schema.sql
@@ -5,10 +5,8 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(250) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     phone VARCHAR(20) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,  -- length = 32? (in case of usage md5 for hasing)
+    password VARCHAR(100) NOT NULL,
     organization VARCHAR(100),
     avatar TEXT DEFAULT 'https://img.icons8.com/?size=100&id=z-JBA_KtSkxG&format=png&color=000000',
     about VARCHAR(300) DEFAULT 'hey I''m using Argus'
 );
-
--- todo: create indexes
